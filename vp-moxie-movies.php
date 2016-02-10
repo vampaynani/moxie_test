97355e6b6a498703645b06a15fc1e865f3d1cd34
vampaynani <vampay@digitaldealers.mx>
2016-02-09 18:13:36 -0600

    initial commit

---
+++ vp-moxie-movies.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Moxie Movies
+ * Description: This plugin adds a JSON API from a custom post type(movies) and displays it as a frontpage.
+ * Version: 1.0.0
+ * Author: Wenceslao Negrete
+ * Author URI: http://github.com/vampaynani
+ */
